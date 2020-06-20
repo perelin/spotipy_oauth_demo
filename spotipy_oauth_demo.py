985769c08e62c0b1e703bfc3e344ab98c07b0f8b
Zachary-Johnston <50839773+Zachary-Johnston@users.noreply.github.com>
2020-06-19 22:55:42 -0700

    Update spotipy_oauth_demo.py

--- spotipy_oauth_demo.py
+++ spotipy_oauth_demo.py
@@ -22,18 +22,18 @@ def index():
     token_info = sp_oauth.get_cached_token()
 
     if token_info:
-        print "Found cached token!"
+        print("Found cached token!")
         access_token = token_info['access_token']
     else:
         url = request.url
         code = sp_oauth.parse_response_code(url)
         if code:
-            print "Found Spotify auth code in Request URL! Trying to get valid access token..."
+            print("Found Spotify auth code in Request URL! Trying to get valid access token...")
             token_info = sp_oauth.get_access_token(code)
             access_token = token_info['access_token']
 
     if access_token:
-        print "Access token available! Trying to get user information..."
+        print("Access token available! Trying to get user information...")
         sp = spotipy.Spotify(access_token)
         results = sp.current_user()
         return results
